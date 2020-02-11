626208a326d4fa4acd1d4e273e73d77fb4eb748c
Moeen Channa <moeenchannah@gmail.com>
2020-02-11 17:10:22 +0500

    Authentication Forms

---
+++ app/src/main/java/com/fyp/emart/project/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.fyp.emart.project.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fyp.emart.project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
