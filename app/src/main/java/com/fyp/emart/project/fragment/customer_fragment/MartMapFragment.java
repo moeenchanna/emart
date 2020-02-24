efc94799c9999475fc8b0ea070a27f31135dd705
Moeen Channa <moeenchannah@gmail.com>
2020-02-24 15:35:03 +0500

    Map show

--- app/src/main/java/com/fyp/emart/project/fragment/customer_fragment/MartMapFragment.java
+++ app/src/main/java/com/fyp/emart/project/fragment/customer_fragment/MartMapFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,9 +13,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fyp.emart.project.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MartMapFragment extends Fragment {
-
+public class MartMapFragment extends Fragment implements OnMapReadyCallback{
+//https://github.com/rrsaikat/MultipleMarker_Using_Volley/blob/master/app/src/main/java/com/rrrsaikat88gmail/multiplemarker_using_volley/MapsActivity.java
+    private GoogleMap mMap;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -21,4 +33,43 @@ public class MartMapFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_mart_map, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMinZoomPreference(15);
+        mMap.setIndoorEnabled(true);
+        mMap.getFocusedBuilding();
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.4233438, -122.0728817))
+                .title("LinkedIn")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.4629101,-122.2449094))
+                .title("Facebook")
+                .snippet("Facebook HQ: Menlo Park"));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.3092293, -122.1136845))
+                .title("Apple"));
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.4233438, -122.0728817), 10));
+
+
+
+    }
 }
