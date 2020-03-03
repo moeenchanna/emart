e57b875cce7d9e18c380abdfb5586ec8a58bd321
saqibmerchant <mahindra88>
2020-03-03 12:32:43 +0500

    add edit text fields of url

--- app/src/main/java/com/fyp/emart/project/fragment/mart_fragment/MartProductFragment.java
+++ app/src/main/java/com/fyp/emart/project/fragment/mart_fragment/MartProductFragment.java
@@ -1,11 +1,6 @@
 package com.fyp.emart.project.fragment.mart_fragment;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +10,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.fyp.emart.project.R;
@@ -29,7 +22,7 @@ public class MartProductFragment extends Fragment implements View.OnClickListene
     private TextInputEditText mPriceProduct;
     private TextInputEditText mQuanProduct;
     private TextInputEditText mCodeProduct;
-    private ImageView mImageView;
+
     private Button mButton;
 
     private static final int STORAGE_PERMISSION_CODE = 123;
@@ -38,6 +31,7 @@ public class MartProductFragment extends Fragment implements View.OnClickListene
     private Uri filePath;
 
     private static final int IMAGE_SELECT_CODE = 1001;
+    private TextInputEditText mImageUrlProduct;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,55 +46,46 @@ public class MartProductFragment extends Fragment implements View.OnClickListene
         mPriceProduct = (TextInputEditText) itemView.findViewById(R.id.product_price);
         mQuanProduct = (TextInputEditText) itemView.findViewById(R.id.product_quan);
         mCodeProduct = (TextInputEditText) itemView.findViewById(R.id.product_code);
-        mImageView = (ImageView) itemView.findViewById(R.id.imageView);
-        mImageView.setOnClickListener(this);
         mButton = (Button) itemView.findViewById(R.id.button);
         mButton.setOnClickListener(this);
+        mImageUrlProduct = (TextInputEditText) itemView.findViewById(R.id.pr_image_url);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == STORAGE_PERMISSION_CODE)
-            if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
-                requestStoragePermission();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == IMAGE_SELECT_CODE) if (resultCode == Activity.RESULT_OK) {
-
-            if (data == null) {
-                //Display an error
-                Toast.makeText(getActivity(), "Unable to handle image.", Toast.LENGTH_SHORT).show();
-                filePath = null;
-                mImageView.setImageResource(R.drawable.selectimage);
-                return;
-            }
-
-            filePath = data.getData();
-            mImageView.setImageURI(filePath);
-
-        } else {
-            filePath = null;
-            mImageView.setImageResource(R.drawable.selectimage);
-        }
-    }
-
-
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//
+//        if (requestCode == STORAGE_PERMISSION_CODE)
+//            if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
+//                requestStoragePermission();
+//    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == IMAGE_SELECT_CODE) if (resultCode == Activity.RESULT_OK) {
+//
+//            if (data == null) {
+//                //Display an error
+//                Toast.makeText(getActivity(), "Unable to handle image.", Toast.LENGTH_SHORT).show();
+//                filePath = null;
+//                mImageView.setImageResource(R.drawable.selectimage);
+//                return;
+//            }
+//
+//            filePath = data.getData();
+//            mImageView.setImageURI(filePath);
+//
+//        } else {
+//            filePath = null;
+//            mImageView.setImageResource(R.drawable.selectimage);
+//        }
+//    }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.imageView:
-               requestStoragePermission();
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, IMAGE_SELECT_CODE);
-                break;
             case R.id.button:
                 Toast.makeText(getActivity(), "Send", Toast.LENGTH_SHORT).show();
                 break;
@@ -109,15 +94,15 @@ public class MartProductFragment extends Fragment implements View.OnClickListene
         }
     }
 
-    private void requestStoragePermission() {
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-            return;
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE))
-            requestStoragePermission();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-
-    }
+//    private void requestStoragePermission() {
+//        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+//            return;
+//
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE))
+//            requestStoragePermission();
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+//            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+//
+//    }
 
 }
