56501b270a32e10910cf7b0dd66e91553e2879b7
Moeen Channa <moeenchannah@gmail.com>
2020-03-19 16:06:29 +0500

    Customer complaints and review fragment created

---
+++ app/src/main/java/com/fyp/emart/project/fragment/customer_fragment/CustomerReviewListFragment.java
@@ -0,0 +1,20 @@
+package com.fyp.emart.project.fragment.customer_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fyp.emart.project.R;
+public class CustomerReviewListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer_review_list, container, false);
+    }
+}
